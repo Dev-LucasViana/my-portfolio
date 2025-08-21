959bac45b99de131bcd58707c72f7a8ba3c3f4d7
lucasviana <nilureis@gmail.com>
2025-08-21 14:26:41 -0300

    Responsividade.

--- script.js
+++ script.js
@@ -2,20 +2,26 @@ const curso1 = document.querySelector(".course-1");
 const desc1 = document.querySelector(".desc-1");
 const curso2 = document.querySelector(".course-2");
 const desc2 = document.querySelector(".desc-2");
+const abrir1 = document.querySelector("#abrir1");
+const abrir2 = document.querySelector("#abrir2");
 
 function click1() {
   if (desc1.style.display === "none" || desc1.style.display === "") {
     desc1.style.display = "flex";
+    abrir1.className = "fa-solid fa-angle-down";
   } else {
     desc1.style.display = "none";
+    abrir1.className = "fa-solid fa-angle-right";
   }
 }
 
 function click2() {
   if (desc2.style.display === "none" || desc2.style.display === "") {
     desc2.style.display = "flex";
+    abrir2.className = "fa-solid fa-angle-down";
   } else {
     desc2.style.display = "none";
+    abrir2.className = "fa-solid fa-angle-right";
   }
 }
 
@@ -25,7 +31,7 @@ const corpo = document.querySelector(".corpo");
 let aux = true;
 function changeTheme() {
   if (aux == true) {
-    corpo.style.backgroundImage = "linear-gradient(270deg, #211f2f, #727a9a)";
+    corpo.style.backgroundImage = "linear-gradient(270deg, #211f2f, #08203e)";
     corpo.style.color = "white";
     theme.className = "fa-solid fa-sun";
     aux = false;
